ca82dece36e64f7c0a49e263f533f11f87918c95
Julio Nico <julionicodantas@gmail.com>
2020-11-22 18:19:14 -0300

    Codigo do pipe

--- acsh.c
+++ acsh.c
@@ -5,32 +5,17 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <errno.h>
-#include <signal.h>
 #include "queue.h"
 #include "acsh.h"
 
+#define READ 0
+#define WRITE 1
+
 // PIDs
 Queue *PIDs;
-// Groups
-Queue *Groups;
 
 // Variável global indicando se a asch está ativa
 int active = 0;
-
-
-// Tratamento dos Sinal Ctrl+C
-static void end_handler_C(int signal) {
-    printf("Não adianta me enviar o sinal por Ctrl-C. Estou vacinado!!\n");
-}
-
-// Tratamento dos Sinal Ctrl+Barra
-static void end_handler_B(int signal) {
-    printf("Não adianta me enviar o sinal por Ctrl-\\ . Estou vacinado!!\n");
-}
-// Tratamento dos Sinal Ctrl+Z
-static void end_handler_Z(int signal) {
-    printf("Não adianta me enviar o sinal por Ctrl-Z . Estou vacinado!!\n");
-}
 
 
 // Tratamento dos comandos, removendo caracteres indesejáveis
@@ -49,6 +34,7 @@ static int parse(char *line, char **argv, int nmax) {
 }
 
 int turn_on() {
+    PIDs = Queue_init(100);
     active = 1;
     printf("\e[H\e[2J"); // limpar tela
     return 1;
@@ -56,6 +42,7 @@ int turn_on() {
 
 int turn_off() {
     active = 0;
+    Queue_del(PIDs);
     return 1;
 }
 
@@ -82,6 +69,8 @@ int get_commands(char *cmd, char **commands) {
 // Separar parametros a partir de um comando
 // Note que o último parâmetro será NULL
 static int get_params(char *command, char **params) {
+    if(!command)
+        return 0;
     int n = parse(command, params, MAX_COMMAND_PARAM+3);
     params[n] = NULL; // último parametro é nulo, de acordo com a função execvp
     return n;
@@ -89,20 +78,26 @@ static int get_params(char *command, char **params) {
 
 // Comandos internos
 // Comparação com o primeiro comando recebido
-static int internal_treatment(char *command, char **params) {
+static int internal_treatment(char **params) {
+    if(!params[0])
+        return 0;
+
     if(strcmp(params[0], "cd") == 0) {
         if(chdir(params[1]) < 0)
             perror("*** ERROR: cd failed\n");
     }
-    else if(strcmp(command, "exit") == 0) // finalizar os outros processos
+    else if(strcmp(params[0], "exit") == 0) {
+        while(!Queue_empty(PIDs)) // finalizar os outros processos
+            kill(dequeue(PIDs), SIGKILL);
         turn_off();
+    }
     else
         return 0;
     return 1;
 }
 
 // Execução de um comando em foreground
-void execute_foreground(char *command, char **params) {
+void execute_foreground(char **params) {
     pid_t pid;
     int status;
     if((pid = fork()) < 0) // cria um processo filho
@@ -116,62 +111,61 @@ void execute_foreground(char *command, char **params) {
 }
 
 // Execução de um comando em background
-
-pid_t execute_background(char *command, char **params, int idGroup) {
+void execute_background(char **params) {
     pid_t pid;
     if((pid = fork()) < 0) // cria um processo filho
         perror("*** ERROR: forking child process failed\n");
     else if(pid == 0) { // para os processos filhos
-        if(idGroup < 0) idGroup = 0; // se o idGroup < 0 entao este é o lider, pgid sera o seu pid
-
-        if(setpgid(0, idGroup) == -1)
-            perror("*** ERROR: failed to set group"); // seta o pgid para pertencer ao grupo dado
-        else if(execvp(*params, params) < 0) // executa o comando
+        if(execvp(*params, params) < 0) // executa o comando
             perror("*** ERROR: exec failed\n");
     }
-
-    // enqueue(PIDs, pid); // guarda o pid do processo criado
-    return pid;
 }
 
 // Execução de uma linha de comando
 void execute_cmd(char *cmd) {
-    size_t i;
-    int m, n;
-    pid_t gid; 
+    size_t i = 0;
+    pid_t pid;
+    int status;
+    int m, n, 
+        first_foreground = 0; // primeiro processo é em foreground?
     char *commands[MAX_COMMANDS];
     char *params[MAX_COMMAND_PARAM+2];
 
     n = get_commands(cmd, commands); // separação dos comandos
-    for(i = 0; i < n; i++) {
-        m = get_params(commands[i], params); // separação dos parâmetros dos comandos
+    m = get_params(commands[0], params); // separação dos parâmetros dos comandos
+    // Internal commands
+    if(n == 1 && internal_treatment(params)) { // cd ou exit?
+        free(commands[0]);
+        return;
+    }
+
+    if(*params[m-1] == '%') {
+        params[m-1] = NULL;
+        first_foreground = 1;
+    }
+    if((pid = fork()) < 0) // cria o primeiro processo
+        perror("*** ERROR: forking child process failed\n");
     
-        if(internal_treatment(commands[i], params)) { // cd ou exit?
-            free(commands[i]);
-            return;
-        }
-        if(*params[m-1] == '%') { // foreground
-            params[m-1] = NULL;
-            execute_foreground(commands[i], params);
-        }
-        else {
+    if(pid == 0) { // primeiro processo
+        setsid(); // altera a sessão
+        if(execvp(*params, params) < 0) // executa o primeiro processo
+            perror("*** ERROR: exec failed\n");
+    }
+    else {
+        if(first_foreground)
+            while(wait(&status) != pid); // aguarda o comando ser completo
 
-            if(i == 0) gid = execute_background(commands[i], params, -1);
-            else execute_background(commands[i], params, gid);
+        enqueue(PIDs, pid); // enfileiramento dos processos principais
 
+        for(i = 1; i < n; i++) { // demais processos
+            m = get_params(commands[i++], params);
+
+            if(*params[m-1] == '%') { // foreground
+                params[m-1] = NULL;
+                execute_foreground(params);
+            }
+            else // background
+                execute_background(params);
         }
-        free(commands[i]);
     }
 }
-
-int sinais(int argc, char *argv[]) {
-    // set handlers
-    if ((signal(SIGINT, end_handler_C) == SIG_ERR) || (signal(SIGQUIT, end_handler_B) ==  SIG_ERR) || (signal(SIGTSTP, end_handler_Z) == SIG_ERR)) {
-        printf("Error while setting a signal handler\n");
-        exit(EXIT_FAILURE);
-    }   
-    while (1) { } // infinite loop
-    return 0;  
-}
-
-
